20c38278fbfaa9b2c94992466cb186f972cea461
EdiedRamos <edieisa22@gmail.com>
2024-12-17 00:31:22 -0500

    feat: complete animation

--- javascript animation effects/text animation effect with javascript/script.js
+++ javascript animation effects/text animation effect with javascript/script.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const message = document.getElementById("message");
   const spans = message.innerText
     .split("")
-    .map((letter, i) => `<span>${letter}</span>`)
+    .map(
+      (letter, i) =>
+        `<span style='transition-delay:${i * 50}ms;filter:hue-rotate(${
+          i * 90
+        }deg)'>${letter}</span>`
+    )
     .join("");
   message.innerHTML = spans;
 });
