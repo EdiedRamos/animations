1311fcfc275bd2c1bc854f5f55ca0170508020ff
EdiedRamos <edieisa22@gmail.com>
2024-12-16 23:59:37 -0500

    feat: update logic

--- javascript animation effects/text animation effect with javascript/script.js
+++ javascript animation effects/text animation effect with javascript/script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const message = document.getElementById("message");
-  // console.log({ message: message.innerText });
-  const newMessage = document.createElement("p");
-  for (const letter of message.innerText) {
-    const span = document.createElement("span");
-    span.innerText = letter;
-    newMessage.appendChild(span);
-  }
-  message.innerHTML = newMessage.innerHTML;
+  const spans = message.innerText
+    .split("")
+    .map((letter, i) => `<span>${letter}</span>`)
+    .join("");
+  message.innerHTML = spans;
 });
